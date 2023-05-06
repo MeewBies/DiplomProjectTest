be45b8f4fe93e3a2ff5979dbfa711d6a25569220
Mort <drunkenracer12@gmail.com>
2023-05-06 09:43:52 +0500

    Add window

---
+++ Win_/winmain_admin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiplomDimaDen.Win_
+{
+    /// <summary>
+    /// Логика взаимодействия для winmain_admin.xaml
+    /// </summary>
+    public partial class winmain_admin : Window
+    {
+        public winmain_admin()
+        {
+            InitializeComponent();
+            DB.NedDB NDB = new DB.NedDB();
+            LVadmin.ItemsSource = NDB.Сотрудники.ToList();
+        }
+    }
+}
